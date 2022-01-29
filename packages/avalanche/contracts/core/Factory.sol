4d4c2910d53c1d66b5659acc35f1c58f61eb8d96
Marton Dobos <dobeis2@gmail.com>
2022-01-29 11:10:13 +0100

    feat: pair & factory contracts, interfaces

---
+++ packages/avalanche/contracts/core/Factory.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "./interfaces/IFactory.sol";
+import "./Pair.sol";
+
+contract Factory is IFactory {
+  address public override feeTo;
+  address public override feeToSetter;
+
+  mapping(address => mapping(address => address)) public override getPair;
+  address[] public override allPairs;
+
+  event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+  constructor(address _feeToSetter) {
+    feeToSetter = _feeToSetter;
+  }
+
+  function allPairsLength() external view override returns (uint256) {
+    return allPairs.length;
+  }
+
+  function createPair(address tokenA, address tokenB) external override returns (address pair) {
+    require(tokenA != tokenB, "The addresses have to be identical!");
+
+    (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+
+    require(token0 != address(0), "Zero address");
+    require(getPair[token0][token1] == address(0), "The pairs already exist"); // single check is sufficient
+
+    bytes memory bytecode = type(Pair).creationCode;
+    bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+    assembly {
+      pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+
+    IPair(pair).initialize(token0, token1);
+    getPair[token0][token1] = pair;
+    getPair[token1][token0] = pair; // populate mapping in the reverse direction
+    allPairs.push(pair);
+    emit PairCreated(token0, token1, pair, allPairs.length);
+  }
+
+  function setFeeTo(address _feeTo) external override {
+    require(msg.sender == feeToSetter, "FORBIDDEN");
+    feeTo = _feeTo;
+  }
+
+  function setFeeToSetter(address _feeToSetter) external override {
+    require(msg.sender == feeToSetter, "FORBIDDEN");
+    feeToSetter = _feeToSetter;
+  }
+}
---
+++ packages/avalanche/contracts/core/interfaces/IPair.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+pragma experimental ABIEncoderV2;
+
+interface IPair {
+  function MINIMUM_LIQUIDITY() external pure returns (uint256);
+
+  function factory() external view returns (address);
+
+  function token0() external view returns (address);
+
+  function token1() external view returns (address);
+
+  function price0CumulativeLast() external view returns (uint256);
+
+  function price1CumulativeLast() external view returns (uint256);
+
+  function kLast() external view returns (uint256);
+
+  function mint(address to) external returns (uint256 liquidity);
+
+  function burn(address to) external returns (uint256 amount0, uint256 amount1);
+
+  function swap(
+    uint256 amount0Out,
+    uint256 amount1Out,
+    address to,
+    bytes calldata data
+  ) external;
+
+  function skim(address to) external;
+
+  function sync() external;
+
+  function initialize(address, address) external;
+}
