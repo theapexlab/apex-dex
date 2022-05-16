8c3412a19cd8e8fbe8db43ffa13107a45bbe69a7
Marton Dobos <dobeis2@gmail.com>
2022-05-16 19:48:12 +0200

    fix: remove unused lines

--- packages/avalanche/contracts/core/ApexERC20.sol
+++ packages/avalanche/contracts/core/ApexERC20.sol
@@ -8,8 +8,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ApexERC20 is ERC20, ERC20Permit {
   string private constant _contractName = "Apex ERC20 Liquidity";
 
-  // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-  bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-
   constructor() ERC20(_contractName, "APX") ERC20Permit(_contractName) {}
 }
